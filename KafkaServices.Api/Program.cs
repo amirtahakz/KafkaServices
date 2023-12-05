48dadf2a7755ea96a997523ca18825b0470e4a8d
amir_taha_kz <amirtaha.zahraei@gmail.com>
2023-12-05 09:47:38 +0330

    Add .net 3.1 nuget package

--- KafkaServices.Api/Program.cs
+++ KafkaServices.Api/Program.cs
@@ -1,6 +1,6 @@
-using KafkaServices.Api.Consumers.Handlers;
-using KafkaServices.Api.Consumers.Models;
-using KafkaServices.Kafka;
+//using KafkaServices.Api.Consumers.Handlers;
+//using KafkaServices.Api.Consumers.Models;
+//using KafkaServices.Kafka;
 
 var builder = WebApplication.CreateBuilder(args);
 var services = builder.Services;
